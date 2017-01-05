7c65da3241df1135e5e0d352ae299761be25ced8
Peter van Nes <peter@reddipped.com>
2017-01-05 23:22:48 +0100

    removal of libreoffice
    Saving another 256MB diskspace

--- scripts/cleanup_packages.sh
+++ scripts/cleanup_packages.sh
@@ -4,7 +4,7 @@
 apt-get -y purge wolfram-engine smbclient samba-common penguinspuzzle minecraft-pi \
 python-pygame pixel-wallpaper Epiphany chromium-browser samba-libs:armhf squeak-vm \
 realvnc-vnc-server realvnc-vnc-viewer nuscratch samba-libs:armhf \
-ruby oracle-java8-jdk nodejs
+ruby oracle-java8-jdk nodejs libreoffice*
 # update to latest raspbian
 #apt-get -y update && apt-get -y dist-upgrade \
 #&& apt-get -y autoremove && apt-get -y autoclean \
